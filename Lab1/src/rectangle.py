594de2b01b10c38676af258393b0eac726413515
vindygao1997 <vindygao1997@gmail.com>
2023-09-12 23:46:33 -0700

    Refactored rectangle class

--- Lab1/src/rectangle.py
+++ Lab1/src/rectangle.py
@@ -5,13 +5,40 @@
  Note that there is no constructor or destructor,
  so a default one will be created for us.
 """
-class Rectangle:
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
     def set_values(self, x, y):
-        self.width = x
-        self.height = y
+        pass
+    
+    @abstractmethod
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, width=0, height=0):
+        self._width = width
+        self._height = height
+    
+    def get_width(self):
+        return self._width
+    
+    def get_height(self):
+        return self._height
+    
+    def set_width(self, width):
+        self._width = width
+        
+    def set_height(self, height):
+        self._height = height
+    
+    def set_values(self, x, y):
+        self._width = x
+        self._height = y
 
     def area(self):
-        return self.width * self.height
+        return self._width * self._height
 
 
 if __name__ == "__main__":
@@ -23,3 +50,4 @@ if __name__ == "__main__":
 
     # Print out the area function
     print("area:", rect.area())
+
