32f9f056dce3c58fa912748a4bd613181899d8de
vindygao1997 <vindygao1997@gmail.com>
2023-10-05 15:04:57 -0700

    refactor gildedrose

--- Assignment3/part1/gilded_rose.py
+++ Assignment3/part1/gilded_rose.py
@@ -11,39 +11,55 @@ class Item:
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
-
-class GildedRose(object):
-
-    def __init__(self, items: list[Item]):
-        # DO NOT CHANGE THIS ATTRIBUTE!!!
+class GildedRose:
+    def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            self.update_item(item)
+
+    def update_item(self, item):
+        if item.name == "Sulfuras, Hand of Ragnaros":
+            self.update_sulfuras(item)
+
+        item.sell_in -= 1  # Decrease sell_in for all items
+
+        if item.name == "Aged Brie":
+            self.update_aged_brie(item)
+        elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+            self.update_backstage_passes(item)
+        elif item.name.find("Conjured"):
+            self.update_conjured_item(item)
+        else:
+            self.update_normal_item(item)
+            
+        # quality of item is never negative
+        item.quality = max(0, item.quality)
+
+    def update_sulfuras(self, item):
+        item.quality = 80 # sulfuras quality should be 80 at all times
+        
+    def update_aged_brie(self, item):
+        item.quality = min(50, item.quality + 1)
+
+    def update_backstage_passes(self, item):
+        if item.sell_in <= 0:
+            item.quality = 0
+        elif item.sell_in <= 5:
+            item.quality = min(50, item.quality + 3)
+        elif item.sell_in <= 10:
+            item.quality = min(50, item.quality + 2)
+        else:
+            item.quality = min(50, item.quality + 1)
+
+    def update_conjured_item(self, item):
+        if item.sell_in < 0:
+            item.quality = max(0, item.quality - 4)
+        else:
+            item.quality = max(0, item.quality - 2)
+    def update_normal_item(self, item):
+        if item.sell_in < 0:
+            item.quality = max(0, item.quality - 2)
+        else:
+            item.quality = max(0, item.quality - 1)
